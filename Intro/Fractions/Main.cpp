c1da3cc02945a51bea6a91734184818965e1b955
Soro_pz45@itstepru.onmicrosoft.com <Soro_pz45@itstepru.onmicrosoft.com>
2021-07-01 20:18:08 +0300

    Fractions

---
+++ Intro/Fractions/Main.cpp
@@ -0,0 +1,198 @@
+﻿#include <iostream>
+using namespace std;
+#define tab "\n \n"
+
+
+class Fraction
+{
+	int integer;      //Целая часть
+	int numerator;    //Числитель
+	int denominator;  //Знаменатель
+public:
+	int get_integer()const
+	{
+		return integer;
+	}
+	int get_numerator()const
+	{
+		return numerator;
+	}
+	int get_denominator()const
+	{
+		return denominator;
+	}
+
+	void set_integer(int integer)
+	{
+		this->integer = integer;
+	}
+	void set_numerator(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denominator(int denominator)
+	{
+		if (denominator == 0)denominator = 1; //фильтрация данных
+		this->denominator = denominator;
+	}
+
+	/////////////////////////////////////Constructors:
+	Fraction()
+	{
+		this->integer = 0;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "Default_Construction: " << this << endl;
+	}
+	Fraction(int integer)
+	{
+		this->integer = integer;
+		this->numerator = 0;
+		this->denominator = 1;
+		cout << "1_Arg_Construction " << this << endl;
+
+
+	}
+	Fraction(int numerator, int denominator)
+	{
+		this->integer = 0;
+		this->numerator = numerator;
+		this->set_denominator(denominator);
+		cout << "2_Arg_Construction: \t" << this << endl;
+
+	}
+
+	Fraction(int integer, int numerator, int denominator)
+	{
+		this->integer = integer;
+		this->numerator = numerator;
+		this->set_denominator(denominator);
+		cout << "3_Arg_Constructor: \t" << this << endl;
+
+	}
+	Fraction(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "Copy_Constructor: " << this << endl;
+	}
+	~Fraction()
+	{
+		cout << "Destructor: \t" << this << endl;
+	}
+	Fraction& operator=(const Fraction& other)
+	{
+		this->integer = other.integer;
+		this->numerator = other.numerator;
+		this->denominator = other.denominator;
+		cout << "Copy_Assignment: \t" << this << endl;
+		return *this;
+	}
+	///////////////////////////////////Incterement/Decrement:
+	Fraction& operator++() //Префиксный инкремент
+	{
+		integer++;
+		return *this;
+	}
+	Fraction operator++(int) // Постфиксный инкремент
+	{
+		Fraction old = *this; // Сохраняем старое значение + Copy_Constructor
+		integer++;
+		return old; //Copy_Constructor
+	}
+
+	///////////////////////////////////Methods:
+	void to_improper()
+	{
+		numerator += integer * denominator;
+		integer = 0;
+	}
+	void to_proper()
+	{
+		integer += numerator / denominator;
+		numerator %= denominator;
+	}
+	void print()const
+	{
+		if (integer)cout << integer;
+		if (integer && numerator) cout << "(";
+		if (numerator)cout << numerator << "/" << denominator;
+		if (integer && numerator)cout << ")";
+		if (integer == 0 && numerator == 0) cout << 0;
+		cout << endl;
+	}
+};
+Fraction operator*(Fraction left, Fraction right)
+{
+	left.to_improper();
+	right.to_improper();
+	/*Fraction result;
+	result.set_numerator(left.get_numerator() * right.get_numerator());
+	result.set_denominator(left.get_denominator() * right.get_denominator());*/
+	//Fraction result
+	//(
+	//	left.get_numerator() * right.get_numerator(),
+	//	left.get_denominator() * right.get_denominator()
+	//);
+	//return result;
+	return Fraction // Вызван явный конструкторб который создает временный объектб с нужным результатом
+	(
+		left.get_numerator() * right.get_numerator(),
+		left.get_denominator() * right.get_denominator()
+	);
+}
+
+int dvaplusdva()
+{
+	return 2 + 2;
+}
+
+//#define CONSTRUCTION_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+#ifdef CONSTRUCTION_CHECK
+
+	Fraction A; //конструктор по-умолчанию
+	A.print();
+	Fraction B(3); //Конструктор с одним параметром
+	B.print();
+	Fraction C(3, 4);
+	C.print();
+	Fraction D(2, 3, 4);
+	D.print();
+	Fraction E = D; //Конструктор копирования
+	E.print();
+	Fraction F; // Конструктор по-умолчанию
+	F = E;      // Оператор =(присваивания) CopyAssignment
+	F.print();
+	cout << dvaplusdva() << endl;
+
+#endif // CONSTRUCTION_CHECK
+
+	Fraction A(2, 3, 4);
+	Fraction B(3, 4, 5);
+	Fraction C = A * B;
+		/*A.print();
+		B.print();
+		A.to_improper();
+		A.print();
+		A.to_proper();
+		A.print();*/
+	C.print();
+
+	for (double i = .25; i < 10; i++)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
+
+	for (Fraction i (1, 4);  i.get_integer () < 10; i++)
+	{
+		i.print();
+	}
+	cout << endl;
+}
--- Intro/Point/Main.cpp
+++ Intro/Point/Main.cpp
@@ -1,13 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 #define tab "\t"
+#define delimiter "\n-----------------------------------------------"
 //#define STRUCT_POINT
+//#define DISTANCE_CHECK
+#define CONSTRUCTORS_CHECK
+//#define ASSIGNMENT_CHECK
+#define CONSTRUCTORS_OVERLOADING
 
 class Point
 {
-public:
 	double x;
 	double y;
+public:
 
 	double get_x()const
 	{
@@ -27,15 +32,67 @@ public:
 	{
 		this->y = y;
 	}
+	 /////////////////////////////////// Constructors:ПЕРЕГРУЗКА КОНСТРУКТОРОВ
 
-	double Lenght(double x, double y)
+	Point() //Конструктор по-умолчанию
 	{
-		double L;
+		x = y = 0;
+		cout << "DefaultConstructor: \t\t" << this << endl;
+	}
+	Point(double x) //Конструктор с одним параметром
+	{
+		this->x = x;
+		this->y = 0;
+		cout << "Single_Argument_Constructor: \t\t" << this << endl;
+	}
+	Point(double x, double y)
+	{
+		this->x = x;
+			this->y = y;
+			cout << "Constructor: \t\t" << this << endl;
+	}
+	Point(const Point& other) //Поверхностное копирование объекта Shallow copy
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "Copy_Constructor: \t\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor: \t\t" << this << endl;
+	}
+
+	////////////////////Operators:
+	Point& operator= (const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "Copy_Assignment: \t\t" << this << endl;
+		return *this;
+	}
+
+
+	void print()const
+	{
+		cout << "X = " << x << tab << "Y = " << y << endl;
+
+	}
+
+	double Distance (Point& other)
+	{
+		//B - this
+		//A - other
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+
+		/*double L;
 		L = sqrt(x * x + y * y);
-		return L;
+		return L;*/
 	}
 };
-double Range_AB(Point A, Point B);
+//double Range_AB(Point A, Point B);
 
 void main()
 {
@@ -54,24 +111,62 @@ void main()
 	Point* pA = &A; //Объвляем указатель на Point и берем адрем точки А
 	cout << pA->x << tab << pA->y << endl;
 	cout << (*pA).x << tab << (*pA).y << endl;
-#endif // STRUCT_POINT
-
-	Point A, B; // создано в поле видимости main и будет уничтожено при завершении функции main
+#endif  STRUCT_POINT
+#ifdef DISTANCE_CHECK
+	Point A;
+	Point B;// создано в поле видимости main и будет уничтожено при завершении функции main
 	
-	cout << "Введите координаты точки A(x): "; cin >> A.x;
-	cout << "Введите координаты точки A(y): "; cin >> A.y;
-	cout << "Введите координаты точки B(x): "; cin >> B.x;
-	cout << "Введите координаты точки B(y): "; cin >> B.y;
+	A.set_x(2);
+	A.set_y(3);
+	B.set_x(4);
+	B.set_y(5);
+	//cout << "Введите координаты точки A(x): "; cin >> A.x;
+	//cout << "Введите координаты точки A(y): "; cin >> A.y;
+	//cout << "Введите координаты точки B(x): "; cin >> B.x;
+	//cout << "Введите координаты точки B(y): "; cin >> B.y;
 
 	
 	cout << A.get_x() << tab << A.get_y() << endl;
-	cout << "Расстояние до точки А: " << A.Lenght(A.x,A.y) << endl;
-	cout << "Расстояние между точкой А и точкой B: " << Range_AB (A,B) << endl;
-		
+	cout << "Расстояние до точки А: " << B.Distance(A) << endl;
+	cout << delimiter << endl;
+	cout << "Расстояние до точки B: " << A.Distance(B) << endl;
+	//cout << "Расстояние между точкой А и точкой B: " << Range_AB (A,B) << endl;
+#endif DISTANCE_CHECK
+
+#ifdef CONSTRUCTORS_CHECK
+	Point A; //Вызывается default constructor
+	A.print();
+	Point B = 3;// Конструктор с одним параметром
+	B.print();
+	Point C(2, 3);
+	C.print();
+	Point D = C; //Copy Constructor
+	D.print();
+	Point E; //Default Constructor
+	E = C;  //Copy Assignment
+	E.print();
+#endif CONSTRUCTORS_CHECK
+
+#ifdef ASSIGNMENT_CHECK
+
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << tab << b << tab << c << endl;
+
+	Point A, B, C;
+	cout << "\n----------------------------------------\n";
+	A = B = C = Point(4, 5);
+	cout << "\n----------------------------------------\n";
+	A.print();
+	B.print();
+	C.print();
+
+#endif ASSIGNMENT_CHECK
+
 }
-double Range_AB(Point A, Point B)
-{
-	double R;
-	R = sqrt(((B.x - A.x) * (B.x - A.x)) + ((B.y - A.y) * (B.y - A.y)));
-	return R;
-}
+////double Range_AB(Point& A, Point& B)
+//{
+//	double R;
+//	R = sqrt(((B.x - A.x) * (B.x - A.x)) + ((B.y - A.y) * (B.y - A.y)));
+//	return R;
+//}
